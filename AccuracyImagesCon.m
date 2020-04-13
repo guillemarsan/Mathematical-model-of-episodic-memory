8c2b083b4ade7b65f5bad060a3ea0ac19b04e069
Guillermo Martín Sánchez <guillemartinsan@gmail.com>
2020-04-13 17:42:04 +0200

    Tried conceptual learning

--- AccuracyImagesCon.m
+++ AccuracyImagesCon.m
@@ -5,7 +5,7 @@
 %
 clear
 close all
-rng(3)
+rng(3) 
 path(path,'Images')
 path(path,'MatFunc/Vision')
 path(path,'MatFunc/Simulate')
@@ -15,7 +15,7 @@ path(path,'MINST')
 %readDigits = 36;
 extra = 400;
 n = extra + 9;           % neuron dimension
-A = 5;           % number of neurons in concept layer
+A = 50;           % number of neurons in concept layer
 M = 200;           % number of neurons in selective layer
 L = 20;
 
@@ -46,10 +46,10 @@ end
 %     s(:,k) = abs(mom(:,k)) / norm(mom(:,k));
 % end
 
-psl = 0.95;       % selective probability
-Th = sqrt(3)*0.5; % selective threshold
-pcn = 0.95;       % concept probability
-Thcn = 0.5;       % concept threshold
+psl = 0.9;       % selective probability
+Th = 1;         % selective threshold
+pcn = 0.9;       % concept probability
+Thcn = 0.1; % concept threshold
 
 alpha = 20;  
 Tmax = 400;       % max integration time
@@ -74,10 +74,11 @@ W0 = W0(:,id);
 %% Do simulations with Option D. Selective layer
 %
 h = 0.005;        % time step (better to decrease)
-d = 150;            % inhibitory coupling
+d = 150;          % inhibitory coupling
+loc = M/20;       % locality of inhibition
 
 % Sensory layer
-W = SimulateNeurons4(Tmax, h, W0, s, f, alpha, b2, Th, d);
+W = SimulateNeurons4Loc(Tmax, h, W0, s, f, alpha, b2, Th, d, loc);
 
 figure;
 V = W'*s;
@@ -91,27 +92,27 @@ ylabel("Stimuli");
 %% Do simulations. Concept layer
 
 y = max(0,W'*s - Th); % compute reaction to s
-y = [y; y; y; y];
+% y = [y; y; y; y];
 % y = y.*(y > epsilon); % avoid round to zero problems
 
-%U0 = 2*rand(M,A) - 1;  % random neurons
-%[~,id] = sort(sum(y'*U0 > Th)); % sort neurons for convenience
-%U0 = U0(:,id);
+U0 = 2*rand(M,A) - 1;  % random neurons
+[~,id] = sort(sum(y'*U0 > Thcn)); % sort neurons for convenience
+U0 = U0(:,id);
 
-%d = 0; %no inhibition
+d = 0; %no inhibition
 
 % Concept layer
-%U = SimulateNeurons3(Tmax, h, U0, y, g, alpha, bcn2, Thcn, d);
+U = SimulateNeurons(Tmax, h, U0, y, g, alpha, bcn2, Thcn, d);
 
 % Manually set the values
-U = zeros(M*4,A);
-Thcn = zeros(A,1);
-for i=0:A-1
-    aux = sum(y(:,i*K+1:i*K+K),2);
-    U(:,i+1) = aux/norm(aux);
-    v = U(:,i+1)'*y(:,i*K+1:i*K+K);
-    Thcn(i+1) = min(v(v~=0));
-end
+% U = zeros(M,A);
+% Thcn = zeros(A,1);
+% for i=0:A-1
+%     aux = sum(y(:,i*K+1:i*K+K),2);
+%     U(:,i+1) = aux/norm(aux);
+%     v = U(:,i+1)'*y(:,i*K+1:i*K+K);
+%     Thcn(i+1) = min(v(v~=0));
+% end
 
 mom2 = zeros(n,5);
 s2 = zeros(n,5);
@@ -125,15 +126,13 @@ end
 V2 = W'*s2;
 y2 = max(0,V2 - Th);
 
-Vcn2 = U'*[y2;y2;y2;y2]
+Vcn2 = U'*y2
 F3 = Vcn2 >= Thcn
-
 
 % Plot raster
 % figure;
 V = U'*y;
 F = V >= Thcn;
-% F2 = V >= Thcn2;
 % R = orderRasterPlot(F');
 figure
 spy(F');
