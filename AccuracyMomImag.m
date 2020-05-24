c88b5d94a2b1f5c9df4ecd39c3d5e16c10c69675
Guillermo Martín Sánchez <guillemartinsan@gmail.com>
2020-05-24 17:24:02 +0200

    Study new filtered stimuli, beta and conceptual inhibition for conceptual performance

--- AccuracyMomImag.m
+++ AccuracyMomImag.m
@@ -1,10 +1,5 @@
 %% Script for testing association: image moments
 %
-% We use the function 'moments' to compute the invariant moments of the 
-% images and use this values as stimuli for the model. 
-% When the concept layer weights U are manually set, the results are 
-% excellent. However, when they are adquired through the learning rule no 
-% desired pattern emerges. 
 %
 
 %% Prepare enviroment
@@ -12,65 +7,66 @@
 clear
 close all
 rng(3) 
-path(path,'Images')
 path(path,'MatFunc/Vision')
-path(path,'MatFunc/Simulate')
 path(path,'MatFunc/Accuracy')
-path(path,'MINST')
+path(path,'MatFunc/Simulate')
+path(path,'MatFunc/Misc')
 
-signa = 400;
-n = signa + 9; % neuron dimension = radius signature steps + 9 moments
-A = 5;            % number of neurons in concept layer
-M = 300;          % number of neurons in selective layer
-L = 20;
+%% Read training data
 
-mom = zeros(n,L);
-s = zeros(n,L);
-Figures = ["One", "Two", "Three", "Four", "Five"];
-k = 1;
-for i = 1:5   
-    for j = 1:4
-        Img = imread(strcat(Figures(i),int2str(j),'.jpeg'));
-        % Compute the moments
-        [mom(1,k),mom(2,k),mom(3:9,k),mom(10:n,k)] = moments(Img,signa,0);
-        s(:,k) = abs(mom(:,k)) / norm(mom(:,k));
-        k = k+1;
-    end
-end
+signa = 400;   % radius signature steps (moments)
+Figures = {'Two','Three','Four','Five','Triangle','One'};
+FLDR = 'Images/MomTrain';
+PlotFLG = false; 
 
-psl = 0.9;       % selective probability
-Th = 1;         % selective threshold
-pcn = 0.9;       % concept probability
-Thcn = 0.1; % concept threshold
+figure('color','w','position',[100 100 900 900])
+mom = ImportImagesEvalMoments(FLDR, Figures, signa, PlotFLG);
 
-alpha = 20;  
+
+%% Sensory stimuli (and angles between them)
+
+s = mom;
+[n,~] = size(s);
+s = sqrt(3/n)*(s - mean(s))./std(s);
+
+nrmS = sqrt(sum(s.^2)); % norma s
+S = s./nrmS;
+CosAngle = S'*S; % cos(angle)
+
+figure('color','w')
+imagesc(CosAngle,[-1 1])
+axis square
+colorbar
+colormap('jet')
+xlabel('stimulus','FontSize',14)
+ylabel('stimulus','FontSize',14)
+title('cosine of the angles among pairs of stimuli','FontSize',14)
+
+%% Do simulations with Option D. Selective layer
+
+M = 300;          % number of neurons in the selective layer
 Tmax = 400;       % max integration time
+Th = 0.8;           % selective threshold
+h = 0.0025;        % time step (better to decrease)
+d = 150;          % inhibitory coupling
+loc = M/20;       % locality of inhibition
+[n,L] = size(mom);  % dimension and number of stimuli
+f = @(t) mod(round(2*t),L)+1;   % function defining the stimulus sequence
+alpha = 20;  
 
-f = @(t) mod(round(t),L)+1;   % function defining the stimulus sequence
+psl = 0.975;       % selective probability
+b2 = 0.7;
 
-K = 4; % associated stimuli. Must be a divisor of readDigits
 
-% function defining the consectutive signals sequence
-g = @(t) mod(round(t),L)+1-mod(round(t),K):mod(round(t),L)+1; 
-
-delta = sqrt(1 - (2*norminv(psl) / sqrt(5*n)));
-b2 = (Th/delta)^2;  % beta^2
-
-bcn2 = (Thcn*sqrt(L)*delta*K*gamma(K + 0.5) / ...
-    (Th*(1-pcn)*(1-delta)*factorial(K-1)*sqrt(M)))^2; % ???
-
+% Set and train the sensory layer with locality
 W0 = 2*rand(n,M) - 1;  % random neurons
 [~,id] = sort(sum(s'*W0 > Th)); % sort neurons for convenience
 W0 = W0(:,id);
 
-%% Do simulations with Option D. Selective layer
-%
-h = 0.005;        % time step (better to decrease)
-d = 150;          % inhibitory coupling
-loc = M/20;       % locality of inhibition
-
-% Sensory layer with locality
 W = SimulateNeurons4Loc(Tmax, h, W0, s, f, alpha, b2, Th, d, loc);
+
+figure('color','w','position',[100 100 1000 600])
+PlotResultsOfSelectiveStratum(s, W0, W, Th)
 
 %% Plot selective layer
 figure;
@@ -78,69 +74,102 @@ V = W'*s;
 F = V > Th;
 R = orderRasterPlot(F');
 spy(R);
+daspect([10 1 100]);
 title("Rasterplot selective layer neurons and stimuli they respond to");
 xlabel("Neurons");
 ylabel("Stimuli");
 
 %% Do simulations. Concept layer
 
+rng(3)
+A = 600;       % number of neurons in the selective layer
+K = 3;          % integration
+Thcn = 0.5;     % conceptual threshold
+% function defining the consectutive signals sequence
+g = @(t) mod(round(t),L)+1-mod(round(t),K):mod(round(t),L)+1; 
+alpha = 20; 
+
+pcn = 0.975; % selective probability
+bcn2 = 0.7;
+
 y = max(0,W'*s - Th); % compute reaction to s
 
 % Hebbian learning
-% U0 = 2*rand(M,A) - 1;  % random neurons
-% [~,id] = sort(sum(y'*U0 > Thcn)); % sort neurons for convenience
-% U0 = U0(:,id);
-% 
-% d = 0; %no inhibition
+U0 = 2*rand(M,A) - 1;  % random neurons
+[~,id] = sort(sum(y'*U0 > Thcn)); % sort neurons for convenience
+U0 = U0(:,id);
+
+loc = A/20;
+d = 150; % no inhibition
+
+% Concept layer
+U = SimulateNeurons4Loc(Tmax, h, U0, y, g, alpha, bcn2, Thcn, d, loc);
+
+% loc = A/20;
+% d = 150; % inhibition
 % 
 % % Concept layer
-% U = SimulateNeurons(Tmax, h, U0, y, g, alpha, bcn2, Thcn, d);
+% U = SimulateNeurons4Loc(Tmax, h, U0, y, g, alpha, bcn2, Thcn, d,loc);
 
 % Manually set the values
-U = zeros(M,A);
-Thcn = zeros(A,1);
-for i=0:A-1
-    aux = sum(y(:,i*K+1:i*K+K),2);
-    U(:,i+1) = aux/norm(aux);
-    v = U(:,i+1)'*y(:,i*K+1:i*K+K);
-    Thcn(i+1) = min(v(v~=0));
-end
+% A = 5;
+% K = 1;
+% U = zeros(M,A);
+% Thcn = zeros(A,1);
+% for i=0:A-1
+%     aux = sum(y(:,i*K+1:i*K+K),2);
+%     U(:,i+1) = aux/norm(aux);
+%     v = U(:,i+1)'*y(:,i*K+1:i*K+K);
+%     Thcn(i+1) = min(v(v~=0));
+% end
 
 %% Plot concept layer
+
 V = U'*y;
 F = V >= Thcn;
+R = orderRasterPlot(F');
 figure
-spy(F');
+spy(R);
+daspect([10 1 100]);
 title("Rasterplot concept layer neurons and stimuli they respond to");
 xlabel("Neurons");
 ylabel("Stimuli");
 
-%% Test examples
-testex = 5;
-mom2 = zeros(n,testex);
-s2 = zeros(n,testex);
-Figures2 = ["One5", "Two5", "Three5", "Four5", "Five5"];
-for i=1:testex
-    Img = imread(strcat(Figures2(i),'.jpeg'));
-    [mom2(1,i),mom2(2,i),mom2(3:9,i),mom2(10:n,i)] = moments(Img,signa,0);
-    s2(:,i) = abs(mom2(:,i)) / norm(mom2(:,i));
-end
+%% Read test examples
+
+Figures = {'One','Two','Three','Triangle','Four','Five'};
+FLDR = 'Images/MomTest';
+PlotFLG = true; 
+
+figure('color','w','position',[100 100 900 900])
+[mom, class] = ImportImagesEvalMoments(FLDR, Figures, signa, PlotFLG);
+
+s2 = mom;
+[n,Lex] = size(s2);
+s2 = sqrt(3/n)*(s2 - mean(s2))./std(s2);
+
+
+%% Compute precision
+
 V2 = W'*s2;
 y2 = max(0,V2 - Th);
 
 Vcn2 = U'*y2;
 F3 = Vcn2 >= Thcn;
+figure
 disp("Reaction of concept layer to test examples:")
-disp(F3)
+spy(F3')
+daspect([10 1 100]);
 
+%% 
 % Compute precision
 error = 0;
-for i=1:testex
-    if predictcon(W,U,s2(:,i),Th,Thcn(i)) ~= i
+for i=1:Lex
+    if predictcon(W,U,s2(:,i),Th,Thcn) ~= i
       error = error + 1;
     end
 end
-prec = 1 - error/testex;
+prec = 1 - error/Lex;
 fprintf("The precision is: %f\n",prec);
 
 
--- AccuracyRawImag.m
+++ AccuracyRawImag.m
@@ -5,131 +5,163 @@
 %
 clear
 close all
-rng(3)
-path(path,'Images')
-path(path,'MatFunc\Simulate')
-path(path,'MatFunc\Patterns')
-path(path,'MatFunc\Accuracy')
-path(path,'MatFunc\Vision')
+rng(3) 
+path(path,'MatFunc/Vision')
+path(path,'MatFunc/Accuracy')
+path(path,'MatFunc/Simulate')
+path(path,'MatFunc/Misc')
 
-n = 20*20;           % neuron dimension
-A = 6;           % number of neurons in concept layer
-M = 200;           % number of neurons in selective layer
-L = 24;          % number of stimuli
+%% Read training data
 
-p = zeros(20,20,L);
-s = zeros(n,L);
-Figures = ["Triangle", "Square", "Circle", "One", "Two", "Three"];
-idx = 1;
-% Plot patterns
-clf
-for i=1:6
-    for j=1:4
-        Img = imread(strcat(Figures(i),int2str(j),'.jpeg'));
-        p(:,:,idx) = imbox(Img,0); % center in the image
-        subplot(L/4,4,idx)
-        showPattern(p(:,:,idx));
-        p(:,:,idx) = p(:,:,idx)/norm(p(:,:,idx)); % normalize
-        aux = p(:,:,idx)';
-        s(:,idx) = aux(:); % linearize
-        idx = idx + 1;
-    end
-end
-sgtitle("Images used");
+Figures = {'One','Two','Three','Four','Five'};
+FLDR = 'Images/RawTrain';
+PlotFLG = true; 
 
-psl = 0.95;       % selective probability
-Th = sqrt(3)*0.5; % selective threshold
-pcn = 0.95;       % concept probability
-Thcn = 0.5;       % concept threshold
+figure('color','w','position',[100 100 900 900])
+[imgs, class] = ImportImagesEvalRaw(FLDR, Figures, PlotFLG);
 
-alpha = 20;  
+
+%% Sensory stimuli (and angles between them)
+
+s = imgs;
+[n,~] = size(s);
+s = sqrt(3/n)*(s - mean(s))./std(s);
+
+nrmS = sqrt(sum(s.^2)); % norma s
+S = s./nrmS;
+CosAngle = S'*S; % cos(angle)
+
+figure('color','w')
+imagesc(CosAngle,[-1 1])
+axis square
+colorbar
+colormap('jet')
+xlabel('stimulus','FontSize',14)
+ylabel('stimulus','FontSize',14)
+title('cosine of the angles among pairs of stimuli','FontSize',14)
+
+%% Do simulations with Option D. Selective layer
+
+M = 300;          % number of neurons in the selective layer
 Tmax = 400;       % max integration time
+Th = 0.8;           % selective threshold
+h = 0.0025;        % time step (better to decrease)
+d = 150;          % inhibitory coupling
+loc = M/20;       % locality of inhibition
+[n,L] = size(imgs);  % dimension and number of stimuli
+f = @(t) mod(round(2*t),L)+1;   % function defining the stimulus sequence
+alpha = 20;  
 
-f = @(t) mod(round(t),L)+1;   % function defining the stimulus sequence
-
-K = 4; % associated stimuli. Must be a divisor of readDigits
-
-% function defining the consectutive signals sequence
-g = @(t) mod(round(t),L)+1-mod(round(t),K):mod(round(t),L)+1; 
-
+psl = 0.975;       % selective probability
 delta = sqrt(1 - (2*norminv(psl) / sqrt(5*n)));
 b2 = (Th/delta)^2;  % beta^2
 
-bcn2 = (Thcn*sqrt(L)*delta*K*gamma(K + 0.5) / ...
-    (Th*(1-pcn)*(1-delta)*factorial(K-1)*sqrt(M)))^2; % ???
 
+% Set and train the sensory layer with locality
 W0 = 2*rand(n,M) - 1;  % random neurons
 [~,id] = sort(sum(s'*W0 > Th)); % sort neurons for convenience
 W0 = W0(:,id);
 
-%% Do simulations with Option D. Selective layer
-%
-h = 0.005;        % time step
-d = 150;            % inhibitory coupling
+W = SimulateNeurons4Loc(Tmax, h, W0, s, f, alpha, b2, Th, d, loc);
 
-% Sensory layer
-W = SimulateNeurons4(Tmax, h, W0, s, f, alpha, b2, Th, d);
+figure('color','w','position',[100 100 1000 600])
+PlotResultsOfSelectiveStratum(s, W0, W, Th)
 
+%% Plot selective layer
 figure;
 V = W'*s;
 F = V > Th;
 R = orderRasterPlot(F');
 spy(R);
+daspect([10 1 100]);
 title("Rasterplot selective layer neurons and stimuli they respond to");
 xlabel("Neurons");
 ylabel("Stimuli");
 
 %% Do simulations. Concept layer
 
+A = 600;       % number of neurons in the selective layer
+K = 2;          % integration
+Thcn = 0.5;     % conceptual threshold
+% function defining the consectutive signals sequence
+g = @(t) mod(round(t),L)+1-mod(round(t),K):mod(round(t),L)+1; 
+alpha = 20; 
+
+pcn = 0.95; % conceptual probability
+% delta = sqrt(1 - (2*norminv(psl) / sqrt(5*n)));
+% bcn2 = (Thcn*sqrt(L)*delta*K*gamma(K + 0.5) / ...
+%     (Th*(1-pcn)*(1-delta)*factorial(K-1)*sqrt(M)))^2; 
+deltacn = sqrt(1 - (2*norminv(psl) / sqrt(5*M)));
+bcn2 = (Th/deltacn)^2;  % beta^2
+
 y = max(0,W'*s - Th); % compute reaction to s
-y = [y; y; y; y];
 
-%U0 = 2*rand(M,A) - 1;  % random neurons
-%[~,id] = sort(sum(y'*U0 > Th)); % sort neurons for convenience
-%U0 = U0(:,id);
+% Hebbian learning
+U0 = 2*rand(M,A) - 1;  % random neurons
+[~,id] = sort(sum(y'*U0 > Thcn)); % sort neurons for convenience
+U0 = U0(:,id);
 
-%d = 0; %no inhibition
+d = 150; %no inhibition
 
 % Concept layer
-%U = SimulateNeurons3(Tmax, h, U0, y, g, alpha, bcn2, Thcn, d);
+U = SimulateNeurons4(Tmax, h, U0, y, g, alpha, bcn2, Thcn, d);
 
 % Manually set the values
-U = zeros(M*4,A);
-Thcn = zeros(A,1);
-for i=0:A-1
-    aux = sum(y(:,i*K+1:i*K+K),2);
-    U(:,i+1) = aux/norm(aux);
-    v = U(:,i+1)'*y(:,i*K+1:i*K+K);
-    Thcn(i+1) = min(v(v~=0));
-end
+% A = 5;
+% K = 1;
+% U = zeros(M,A);
+% Thcn = zeros(A,1);
+% for i=0:A-1
+%     aux = sum(y(:,i*K+1:i*K+K),2);
+%     U(:,i+1) = aux/norm(aux);
+%     v = U(:,i+1)'*y(:,i*K+1:i*K+K);
+%     Thcn(i+1) = min(v(v~=0));
+% end
 
-%% Test accuracy
-
-p = zeros(20,20,5);
-s2 = zeros(n,5);
-Figures2 = ["Triangle5", "Square5", "Circle5", "One5", "Two5", "Three5"];
-figure;
-for i=1:6
-    Img = imread(strcat(Figures2(i),'.jpeg'));
-    p(:,:,i) = imbox(Img,0);
-    subplot(3,2,i)
-    showPattern(p(:,:,i));
-    p(:,:,i) = p(:,:,i)/norm(p(:,:,i)); % normalize
-    aux = p(:,:,i)';
-    s2(:,i) = aux(:); % linearize
-end
-V2 = W'*s2;
-y2 = max(0,V2 - Th);
-
-Vc2 = U'*[y2;y2;y2;y2]
-F3 = Vc2 >= Thcn
-
-%% Plot concept raster
-
+%% Plot concept layer
 V = U'*y;
 F = V >= Thcn;
+R = orderRasterPlot(F');
 figure
-spy(F');
+spy(R);
+daspect([10 1 100]);
 title("Rasterplot concept layer neurons and stimuli they respond to");
 xlabel("Neurons");
 ylabel("Stimuli");
+
+return
+%% Read test examples
+
+Figures = {'One','Two','Three','Four','Five'};
+FLDR = 'Images/RawTest';
+PlotFLG = true; 
+
+figure('color','w','position',[100 100 900 900])
+[imgs, class] = ImportImagesEvalRaw(FLDR, Figures, PlotFLG);
+
+s2 = imgs;
+[n,Lex] = size(s2);
+s2 = sqrt(3/n)*(s2 - mean(s2))./std(s2);
+
+
+%% Compute precision
+
+V2 = W'*s2;
+y2 = max(0,V2 - Th);
+
+Vcn2 = U'*y2;
+F3 = Vcn2 >= Thcn;
+figure
+disp("Reaction of concept layer to test examples:")
+spy(F3')
+daspect([10 1 100]);
+
+% Compute precision
+error = 0;
+for i=1:Lex
+    if predictcon(W,U,s2(:,i),Th,Thcn) ~= i
+      error = error + 1;
+    end
+end
+prec = 1 - error/Lex;
+fprintf("The precision is: %f\n",prec);
