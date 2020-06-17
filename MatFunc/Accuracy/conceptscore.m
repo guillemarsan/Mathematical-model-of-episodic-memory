394bdb1b56dd099cb03bdc051b54ac55b9bcba5c
Guillermo Martín Sánchez <guillemartinsan@gmail.com>
2020-06-17 23:03:49 +0200

    Properly defined concept score

--- MatFunc/Accuracy/conceptscore.m
+++ MatFunc/Accuracy/conceptscore.m
@@ -1,10 +1,11 @@
-function score = conceptscore(dict)
+function score = conceptscore(dict,n)
     % Function which, given the dictionary of conceptual neurons computes a
     % 'score' 
     %
     % Inputs
     %   dict = 1xa array where each value is the concept each conceptual
     %   neuron associates. -1 if it is not conceptual
+    %   n = number of concepts we expect it to learn
     % Outputs
     %   score = integer between 0 and n+1 as follows:
     %       0: no concepts are learned
@@ -15,19 +16,19 @@ function score = conceptscore(dict)
     concepts = unique(dict);
     A = length(dict);
     minn = A/20;
-    newcon = A;
+    %newcon = A;
     score = 0;
     for i = 2:length(concepts)
         numcon = sum(dict == concepts(i));
         if numcon > minn
             score = score + 1;
         end
-        if numcon < newcon
-            newcon = numcon;
-        end
+%         if numcon < newcon
+%             newcon = numcon;
+%         end
     end
-    if score == length(concepts)-1
-        if sum(dict == -1) > newcon
+    if score == n
+        if sum(dict == -1) > minn
             score = score+1;
         end
     end
